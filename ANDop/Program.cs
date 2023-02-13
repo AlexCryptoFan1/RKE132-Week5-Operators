200b2437987088b84ef4a6362eb904a433462b4f
Aleksandr Utkin <utkin.aleksandr123@gmail.com>
2023-02-13 21:50:40 +0200

    Week5 code

---
+++ ANDop/Program.cs
@@ -0,0 +1,19 @@
+﻿//Math rohkem 90, Biologia rohkem 90...
+
+int math, biology, chemistry;
+
+Console.WriteLine("Math res");
+math = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Bio res");
+biology = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Chem res");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if(math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Годен");
+}    
+else
+{
+    Console.WriteLine("Ur application is not approved");
+}    
---
+++ ORop/Program.cs
@@ -0,0 +1,22 @@
+﻿//math r90 OR Chem r90 OR Bio r90
+
+//math && chem || math && bio || chem && bio
+int math, biology, chemistry;
+
+Console.WriteLine("Math res");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("BIo res");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Chem res");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if((math >= 90 && chemistry >= 90) || (math >= 90 && biology >=90) || (chemistry >= 90 && biology >=90))
+{
+    Console.WriteLine("Sobib");
+}
+else
+{
+    Console.WriteLine("Ei sobi");
+}
